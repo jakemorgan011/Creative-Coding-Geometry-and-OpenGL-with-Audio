c23f574999269c245fd9de9250dee80b234906e0
jakemorgan <jakemorgancoolstuff@gmail.com>
2025-10-28 09:52:25 -0700

    cross compatibility for InSp

--- Infinite Space/build.sh
+++ Infinite Space/build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" ]]; then
+  IS_WINDOWS=true
+elif [[ "$(uname -s)" == "MINGW"* || "$(uname -s)" == "MSYS"* ]]; then
+  IS_WINDOWS=true
+else
+  IS_WINDOWS=false
+fi
+
+echo "TERMITE - OS Found: $(if $IS_WINDOWS; then echo "Windows"; else echo "Unix Like"; fi)"
+
 if [ -d "build" ]; then
   echo "TERMITE - build folder already exists."
 else
@@ -14,9 +26,44 @@ else
   git submodule update --init --recursive
 fi
 
-# build
+
+
+#build
 cd build
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ --no-warn-unused-cli -S "../" -B "../build" -G "Unix Makefiles"
+
+# windows
+if $IS_WINDOWS; then
+  echo "TERMITE - config for windows"
+  cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+    --no-warn-unused-cli \
+    -S "../" \
+    -B "../build" \
+    -G "Visual Studio 17 2022"
+
+  if [ $? -ne 0]; then
+    echo "TERMITE - No Visual Studio found, trying Ninja build"
+    cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+      --no-warn-unused-cli \
+      -S "../" \
+      -B "../build" \
+      -G "Ninja"
+  fi
+# unix
+else
+  echo "TERMITE - config for Unix-like"
+  cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+    -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang \
+    -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ \
+    --no-warn-unused-cli -S "../" \
+    -B "../build" \
+    -G "Unix Makefiles"
+fi
+
+
+
 cd ..
 cmake --build "build" --config Debug --target all -j 11 
 
